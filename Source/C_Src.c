8f0fddcc75fa1ac585d1a97ee29182a5fd1b08dc
HondaShio <ScMason90@yandex.com>
2026-09-08 17:06:34 +0800

    Add:HondaShio mentioned

--- Source/C_Src.c
+++ Source/C_Src.c
@@ -4,5 +4,7 @@ int main()
 {
     printf("Sc_Mason90: C_Src.c: Hello, World! \n");
     printf("Sc_Mason90: New printf from upstream. \n");
+    printf("HondaShio mentioned C_Src.c: Hello, World! \n");
+
     return 0;
 }
--- Source/src.cpp
+++ Source/src.cpp
@@ -4,5 +4,6 @@ int main()
 {
     std::cout << "Sc_Mason90: src.cpp: Hello, world!" << std::endl;
     std::cout << "Sc_Mason90: New std::cout from upstream." << std::endl;
+    std::cout << "HondaShio mentioned src.cpp: Hello, world!" << std::endl;
     return 0;
 }
